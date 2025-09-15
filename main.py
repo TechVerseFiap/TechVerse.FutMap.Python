7164e52e48998b11b99ca3d6bf365fd6630363ee
DudaFror <dudacastrocs@gmail.com>
2025-09-15 15:53:53 -0300

    [FEAT] Interactive Menu added into the project

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def login_usuarios(email, senha):
     return False, """Email e senha inválidos. Tente novamente ou cadastr-se caso ainda não tenha uma conta!"""
 
 def cadastro_eventos(nome, sub, cidade, data):
-    evento = {"nome": nome, "sub(idade)": sub, "cidade": cidade, "data(DD/MM/AAAA)": data}
+    evento = {"nome": nome, "sub(idade)": sub, "cidade": cidade, "data": data}
     eventos.append(evento)
     return """Evento '{nome}' cadastrado com sucesso e ocorrerá na cidade '{cidade}' em '{data}'!"""
 
@@ -24,3 +24,46 @@ def listar_eventos():
     for i, e in enumerate(eventos, 1):
         resultado += f"{i}, {e['nome']} - ({e['sub']}) - ({e['cidade']}) - {e['data']} /n"
         return resultado
+    
+def menu():
+    while True:
+        print("\n Sistema Futebol Feminino ")
+        print("1. Cadastrar Usuária")
+        print("2. Login")
+        print("3. Cadastrar Evento")
+        print("4. Listar Eventos")
+        print("0. Sair")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            nome = input("Nome: ")
+            email = input("Email: ")
+            senha = input("Senha: ")
+            ok, msg = cadastro_usuarios(nome, email, senha)
+            print(msg)
+
+        elif opcao == "2":
+            email = input("Email: ")
+            senha = input("Senha: ")
+            ok, msg = login_usuarios(email, senha)
+            print(msg)
+
+        elif opcao == "3":
+            nome = input("Nome do evento: ")
+            sub = input("Categoria de idade das jogadoras: ")
+            cidade = input("Cidade: ")
+            data = input("Data (DD/MM/AAAA): ")
+            print (cadastro_eventos(nome, sub, cidade, data))
+
+        elif opcao == "4":
+            print(listar_eventos())
+        
+        elif opcao == "5":
+            print("Saindo do sistema, agradecemos por estar aqui!")
+            break
+
+        else:
+            print ("Opção inválida, insira um número de 1 a 5 e tente novamente.")
+
+if __name__ == "__main__":
+    menu()
